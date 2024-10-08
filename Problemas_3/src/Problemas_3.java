dccfc530908053e5ee5df827650b08666047d831
Lucelo <zeroestrellasazules@gmail.com>
2024-10-08 14:32:00 +0200

    Día 08/10/24

---
+++ Problemas_3/src/Problemas_3.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Problemas_3 {
+    public static void main(String[] args) {
+        Scanner valor=new Scanner(System.in);
+        System.out.println();
+
+    }
+}
--- Prueba/src/Prueba_1.java
+++ Prueba/src/Prueba_1.java
@@ -1,19 +1,31 @@
-import java.util.Scanner;
-
 public class Prueba_1 {
 
-    public static void main (String[] args) {
-        private static Scanner teclado = new Scanner(System.in);
+    public static void main(String[] args) {
+        int n1 = 1, n2 = 2, n3 = 1;
 
-        System.out.println("Introduce un numero:");
-        num1=Integer.parseInt(teclado.nextLine());
+        if (n1 >= n2 && n1 >= n3) {
+            // n1 es el mayor
+            if (n2 >= n3) {
+                // El orden es n1 >= n2 >= n3
 
-        System.out.println("Introduce un numero:");
-        num2=Integer.parseInt(teclado.nextLine());
-
-        System.out.println("Introduce un numero:");
-        num3=Integer.parseInt(teclado.nextLine());
-
+            } else {
+                // El orden es n1 >= n3 >= n2
+            }
+        } else if (n2 >= n1 && n2 >= n3) {
+            // n2 es el mayor
+            if (n1 >= n3) {
+                // El orden es n2 >= n1 >= n3
+            } else {
+                // El orden es n2 >= n3 >= n1
+            }
+        } else {
+            // n3 es el mayor
+            if (n1 >= n2) {
+                // El orden es n3 >= n1 >= n2
+            } else {
+                // El orden es n3 >= n2 >= n1
+            }
+        }
 
     }
 }
